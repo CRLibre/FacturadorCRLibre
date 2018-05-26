a507a35c03518ddce956562ed79eccdd9f9aa0e2
julian <julian@evoric.com>
2018-05-26 15:28:35 -0600

    tmp screens login y menu

--- src/crLibreFront.js
+++ src/crLibreFront.js
@@ -2,12 +2,33 @@ function crLibreFront_boot(){
     crLibreApi_checkLogin(function(){
         crLibreFront_debug("Logueado");
         //Menu principal
+        crLibreFront_menuScreen();
         
     }, function(){
         crLibreFront_debug("No logueado");
         //Ventana de login
+        crLibreFront_loginScreen();
     });
 }
+
+function crLibreFront_loginScreen(){
+    //Login screen
+    
+    var loginScreen = "html";
+    
+    crLibreFront_setContent(loginScreen);
+    
+}
+
+function crLibreFront_menuScreen(){
+    //Login screen
+    
+    var loginScreen = "html";
+    
+    crLibreFront_setContent(loginScreen);
+    
+}
+
 
 /**********************************/
 /*Function to set content         */
